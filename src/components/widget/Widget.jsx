1d9bea4ddf28f786ed2c86c2bee01b131ad2308a
RizalReza23 <rizalrezaroebojo18@gmail.com>
2024-04-17 12:59:18 +0700

    ubah teks di app

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -2,8 +2,11 @@ import "./widget.scss";
 
 const Widget = () => {
   return (
-    <div>Widget</div>
-  )
-}
+    <div className="widget">
+        <div className="left">left</div>
+        <div className="left">right</div>
+    </div>
+  );
+};
 
-export default Widget
+export default Widget;
