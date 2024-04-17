a6a8e3403f69b307b36e500a51dd28b4a6be6f7c
RizalReza23 <rizalrezaroebojo18@gmail.com>
2024-04-17 13:09:13 +0700

    ubah teks di app

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,22 @@
 import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
 const Widget = () => {
   return (
     <div className="widget">
-        <div className="left">left</div>
-        <div className="left">right</div>
+      <div className="left">
+        <div className="title">USERS</div>
+        <div className="counter">512</div>
+        <div className="link">See all users</div>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          10%
+        </div>
+        <PersonOutlinedIcon />
+      </div>
     </div>
   );
 };
