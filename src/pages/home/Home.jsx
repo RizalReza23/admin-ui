90c7fee2f42e5f31f575b3469a080a398f52ffe5
RizalReza23 <rizalrezaroebojo18@gmail.com>
2024-04-23 14:21:21 +0700

    ubah teks di app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -14,7 +15,10 @@ const Home = () => {
             <Widget type="order"/>
             <Widget type="earning"/>
             <Widget type="balance"/>
-        
+        </div>
+        <div className="charts">
+          <Chart type="order" aspect={2 / 1} />
+          <Chart type="earning" aspect={2 / 1} />
         </div>
         </div>
     </div>
